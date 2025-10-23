472ad4b6e0787b9e64ffe093e53180ee16be3b40
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-10-23 15:05:26 -0700

    main.cpp
    Core header is being found.
    
    Now, we will test a SIMPLE call to it

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
-int main() 
+// Just test that we can include JUCE headers without linking
+// Don't actually use any JUCE functionality that requires linking
+#include <juce_core/juce_core.h>
+
+int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "JUCE headers compiled successfully!" << std::endl;
+
+    // Don't create any JUCE objects that would require linking
+    // Just use the version macros which are compile-time only
+    //std::cout << "JUCE version: " << JUCE_MAJOR_VERSION << "." << JUCE_MINOR_VERSION << std::endl;
+
     return 0;
 }
