9553ef51e700a3aba77c249ed1e973b087e2bbb5
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-10-24 12:36:45 -0700

    main.cpp
    This fetches us what goes wrong in abuild. It's not a lot left to patch up, nothing that's a critical problem, just the same bullshit I've been seeing for the past couple of hours

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdio>
+#include <memory>
+#include <array>
 
 //#include <juce_core/juce_core.h>
 
@@ -7,6 +10,25 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     std::cout << "JUCE headers compiled successfully!" << std::endl;
 
+    /*
+    We use this as a means of fetching our abuild, so we can see where things
+    are failing. This helps since it lets us know if all the headers exist
+    */
+    const char* cmd = "abuild -r 2>&1";
+
+    std::array<char, 128> buffer;
+    std::unique_ptr<FILE, decltype(&pclose)> 
+    pipe(popen(cmd, "r"), pclose);
+    if (!pipe) 
+    {
+        std::cerr << "Failed to run abuild" << std::endl;
+        return 1;
+    }
+
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) 
+    {
+        std::cout << buffer.data();
+    }
 
     return 0;
 }
