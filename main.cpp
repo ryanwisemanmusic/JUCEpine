366e3e49d24d35bc4668f388533d9b3f403661ec
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-10-29 15:27:18 -0700

    main.cpp
    Since you can see the abuild process, no need to fetch it here.
    
    Currently need to resolve the issues of headers because there's a lack of it being in release mode

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
 #include <iostream>
-#include <cstdio>
-#include <memory>
-#include <array>
 
+// Just include and check that JUCE headers exist
 //#include <juce_core/juce_core.h>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "JUCE headers compiled successfully!" << std::endl;
-
-    /*
-    We use this as a means of fetching our abuild, so we can see where things
-    are failing. This helps since it lets us know if all the headers exist
-    */
-    const char* cmd = "abuild -r 2>&1";
-
-    std::array<char, 128> buffer;
-    std::unique_ptr<FILE, decltype(&pclose)> 
-    pipe(popen(cmd, "r"), pclose);
-    if (!pipe) 
-    {
-        std::cerr << "Failed to run abuild" << std::endl;
-        return 1;
-    }
-
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) 
-    {
-        std::cout << buffer.data();
-    }
-
+    std::cout << "Test program running!" << std::endl;
     return 0;
 }
