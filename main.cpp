619c4ae075a5e26aafd86224a058b636494a85d7
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-11-04 12:27:30 -0800

    main.cpp
    All headers can be called, and their libraries should be linked. Will test out functionality on Friday

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
-//Foundational headers
-#include <iostream>
+// Foundational headers
 #include <juce_core/juce_core.h>
 #include <juce_events/juce_events.h>
 #include <juce_data_structures/juce_data_structures.h>
+#include <juce_cryptography/juce_cryptography.h>
+#include <juce_analytics/juce_analytics.h>
 
-//Audio headers
+// Audio headers
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_audio_plugin_client/juce_audio_plugin_client.h>
+#include <juce_dsp/juce_dsp.h>
 
-//Graphics headers
+// Graphics & GUI headers
 #include <juce_graphics/juce_graphics.h>
 #include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_opengl/juce_opengl.h>
 
+// Additional modules 
+#include <juce_box2d/juce_box2d.h>
+#include <juce_osc/juce_osc.h>
 
 void juce_core() {
     std::cout << "=== JUCE Core Module Test ===" << std::endl;
