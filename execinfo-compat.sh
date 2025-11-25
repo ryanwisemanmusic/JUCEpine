8e9098567d7c736b94718b718b49541ffc85fe94
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-11-24 21:47:27 -0800

    execinfo-compat.sh
    This is some foundational handling of the backtrace functionality. And so this is a file that will be part of my submission to Alpine. Yay!

---
+++ execinfo-compat.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Create execinfo.h compatibility header for Alpine musl
+install -Dm644 /dev/stdin "$builddir/modules/juce_core/juce_core/execinfo.h" <<'EOF'
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if defined(__has_include)
+  #if __has_include(<elfutils/libdwfl.h>)
+  #endif
+#endif
+#ifndef _EXECINFO_H
+#define _EXECINFO_H 1
+int backtrace(void **buffer, int size) __attribute__((weak));
+char **backtrace_symbols(void *const *buffer, int size) __attribute__((weak));
+void backtrace_symbols_fd(void *const *buffer, int size, int fd) __attribute__((weak));
+
+__attribute__((weak))
+int backtrace(void **buffer, int size) {
+    (void)buffer; (void)size;
+    return 0;
+}
+
+__attribute__((weak))
+char **backtrace_symbols(void *const *buffer, int size) {
+    (void)buffer; (void)size;
+    return (char**)0;
+}
+
+__attribute__((weak))
+void backtrace_symbols_fd(void *const *buffer, int size, int fd) {
+    (void)buffer; (void)size; (void)fd;
+}
+
+#endif /* _EXECINFO_H */
+
+#ifdef __cplusplus
+}
+#endif
+EOF
+
+# Fix execinfo.h includes throughout JUCE modules
+find "$builddir/modules" -type f \( -name "*.cpp" -o -name "*.h" \) -exec \
+    sed -i 's|#include <execinfo.h>|#include "juce_core/execinfo.h"|g' {} \;
